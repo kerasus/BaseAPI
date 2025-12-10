a4a44b09f09cf8039bec27835b3cb5a1c508990f
ali <a.esmaeili@asredanesh.com>
2025-12-10 15:09:33 +0330

    rename package

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { default as BaseAPI } from './BaseAPI'
-export type { ListType, Endpoints } from './BaseAPI'
+export { default as APIBaseKit } from './APIBaseKit'
+export type { ListType, Endpoints } from './APIBaseKit'
 
